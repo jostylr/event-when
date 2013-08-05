fd20dd0b84af7059e9719ee7078dd672efb909aa
James Taylor <jostylr@gmail.com>
2013-08-05 10:01:54 -0400

    first example for debugging

---
+++ examples/simple.js
@@ -0,0 +1,20 @@
+var EventWhen = require('../index.js');
+
+var emitter = new EventWhen();
+
+emitter.greeting = "hi dude";
+
+emitter.on("test 1 on", function () {
+    console.log("test 1 starts");
+    emitter.emit("test 1 finishes");
+});
+
+f = emitter.last;
+
+emitter.on("test 1 finsihes", function () {
+    console.log("test 1 finishes");
+    emitter.off("test 1 on", f);
+    emitter.emit("test 1 on");
+});
+
+emitter.emit("test 1 on");
