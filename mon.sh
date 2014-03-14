92167d0ad16a71e015329fa9826e83ec6ca6635b
James Taylor <jostylr@gmail.com>
2014-03-14 17:52:05 -0400

    watcher updated to say pass after fail is fixed

--- mon.sh
+++ mon.sh
@@ -6,8 +6,9 @@ cd test
 node ../node_modules/.bin/literate-programming test.md
 echo "testing"
 node testrunner.js
+rc=$?
 echo "done"
 cd ..
-
+exit $rc
 # use with nodemon -e md --ignore *.js --ignore README.md --ignore TODO.md --exec ./mon.sh
 # make sure mon.sh is executable: chmod +x mon.sh
--- watcher.js
+++ watcher.js
@@ -2,12 +2,26 @@ var fs = require('fs');
 var exec = require('child_process').exec,
     child;
 
+var flag = false;
+var count = 0;
+
 var out = function (error, stdout, stderr) {
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
-    if (error !== null) {
-      console.log('exec error: ' + error);
+    if (error !== null) { 
+        if ( (flag === false) || (count > 10) ) {
+            flag = true; 
+            count = 1; 
+            exec('say -v Cellos "fail"', function () {});
+        } else {
+            count += 1;
+        }
+    } else if (flag === true) {
+        exec('say -v Vicki "pass"', function () {});
+        flag = false;
+        count = 0;
     }
+    console.log("------\ndone", new Date()); 
 };
 
 fs.watchFile("eventwhen.md", function () {
